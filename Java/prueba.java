6e8884b00ef551e6f3bc06fbdaaa27e8de5bb6d1
Jesus Sanchez <chusete777@gmail.com>
2022-03-02 09:27:11 +0100

    creacion y subida de primeros archivos

---
+++ Java/prueba.java
@@ -0,0 +1,44 @@
+package hola;
+
+
+
+public class hola{ 
+
+    static void iii(){
+        int x=0;
+        while (x<2){
+            for(int i=0;i<10;i++){
+                System.out.print("I");
+            }
+            System.out.println();
+            x++;
+        }
+        x=0;
+        while (x<4){
+            
+            System.out.println("    II");
+            
+           // System.out.println();
+            x++;
+        }  
+        x=0;
+        while (x<2){
+            for(int i=0;i<10;i++){
+                System.out.print("I");
+            }
+            System.out.println();
+            x++;
+        }    
+    }
+    static void pp(){
+        for(int i=0;i<8;i++){
+            System.out.println("PP");
+        }
+    }
+public static void main(String[] args) {
+iii();
+pp();
+
+
+}
+}
