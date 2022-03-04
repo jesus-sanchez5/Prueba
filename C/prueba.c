762a37a7554d3bc35c83e3b9170004629c872c6e
Jesus <chusete777@gmail.com>
2022-03-04 14:10:46 +0100

    cambios ene prueba

--- C/prueba.c
+++ C/prueba.c
@@ -2,7 +2,12 @@
 
 void main(){
 
-printf("IIIIIIII\tPPPPPPPP\n   II   \tPP    PP\n   II   \tPPPPPPPP\n   II   \tPP\n   II   \tPP\nIIIIIIII\tPP\n");
-printf("Holaaa");
+printf("IIIIIIII  PPPPPPPP\n   II     PP    PP\n   II     PPPPPPPP\n   II     PP\n   II     PP\nIIIIIIII  PP\n");
+printf("\n\n\n");
+printf(" SSSSSSS   CCCCCCC\tAAAAAAAA  NN    NN\nSSS \t  CCC\t\tAA    AA  NNN   NN\nSSSSSS\t  CCC\t\tAA    AA  NN N  NN\n  SSSSSS  CCC\t\tAAAAAAAA  NN  N NN\n    SSSS  CCC\t\tAA    AA  NN   NNN\nSSSSSSS\t   CCCCCCC\tAA    AA  NN    NN\n");
+printf("\n\n\n");
+printf("IIIIIIII  PPPPPPPP    SSSSSSS   CCCCCCC\tAAAAAAAA  NN    NN\n   II     PP    PP   SSS \t  CCC\t\tAA    AA  NNN   NN\n   II     PPPPPPPP   SSSS  \t  CCC\t\tAA    AA  NN N  NN\n   II     PP\t        SSSS  CCC\t\tAAAAAAAA  NN  N NN\n   II     PP\t         SSS  CCC\t\tAA    AA  NN   NNN\nIIIIIIII  PP\t    SSSSSSS\t   CCCCCCC\tAA    AA  NN    NN\n");
+
+
 
 }
