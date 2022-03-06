e5a76ac4fa929cc83fa07e75e97ca54fa3346267
Jesus <chusete777@gmail.com>
2022-03-06 12:25:59 +0100

    cambios

--- C/Windows/prog.c
+++ C/Windows/prog.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
-#define CYAN_T     "\x1b[36m"
+
 
 int main(int argc, char *argv[]){   
     if(argc==2){
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]){
     system("PAUSE");  }
     else{
        
-       printf (CYAN_T "IIIIIIII  PPPPPPPP    SSSSSSS   CCCCCCC\tAAAAAAAA  NN    NN\n   II     PP    PP   SSS       CCC\tAA    AA  NNN   NN\n   II     PPPPPPPP   SSSS      CCC\tAA    AA  NN N  NN\n   II     PP\t        SSSS   CCC\tAAAAAAAA  NN  N NN\n   II     PP\t         SSS   CCC\tAA    AA  NN   NNN\nIIIIIIII  PP\t    SSSSSSS     CCCCCCC\tAA    AA  NN    NN\n\n\n");
+       printf ("IIIIIIII  PPPPPPPP    SSSSSSS   CCCCCCC\tAAAAAAAA  NN    NN\n   II     PP    PP   SSS       CCC\tAA    AA  NNN   NN\n   II     PPPPPPPP   SSSS      CCC\tAA    AA  NN N  NN\n   II     PP\t        SSSS   CCC\tAAAAAAAA  NN  N NN\n   II     PP\t         SSS   CCC\tAA    AA  NN   NNN\nIIIIIIII  PP\t    SSSSSSS     CCCCCCC\tAA    AA  NN    NN\n\n\n");
 
         system("PAUSE");
         }
